af80be4a466d3635711b942d52a2980f78f8a047
Pieter Custers <pieter@dexterenergy.ai>
2026-04-20 07:52:24 +0200

    feat(quick-260420-ak5): wire NewProjectModal into app.py and screens __init__
    - screens/__init__.py exports NewProjectModal and NewProjectResult
    - app.py action_new_project() uses NewProjectModal(repos=self._repos)
    - Project created with repo and branch pre-filled from modal result
    - NameInputModal import retained (still used by rename flows)

--- src/joy/screens/__init__.py
+++ src/joy/screens/__init__.py
@@ -2,6 +2,7 @@ from joy.screens.archive_browser import ArchiveBrowserModal
 from joy.screens.confirmation import ConfirmationModal
 from joy.screens.legend import LegendModal
 from joy.screens.name_input import NameInputModal
+from joy.screens.new_project import NewProjectModal, NewProjectResult
 from joy.screens.preset_picker import PresetPickerModal
 from joy.screens.repo_picker import RepoPickerModal
 from joy.screens.settings import SettingsModal
@@ -12,6 +13,8 @@ __all__ = [
     "ConfirmationModal",
     "LegendModal",
     "NameInputModal",
+    "NewProjectModal",
+    "NewProjectResult",
     "PresetPickerModal",
     "RepoPickerModal",
     "SettingsModal",
